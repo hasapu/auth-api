845e0d0f5a4797fd6bca6b4f33f50528e6851993
hasapu <hari.sakti.putra@gmail.com>
2018-10-06 21:45:43 +0700

    localhost direct to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,21 @@ import {CoreRouting} from './core/core.routing';
 import {LoanComponent} from './loan/loan.component';
 import {AuthGuard} from './core/auth/auth.guard';
 import {LoanCreateComponent} from './loan/create/loan.create.component';
+import {AppComponent} from './app.component';
 
 const routes: Routes = [
-  { path: 'loan',
-    component: LoanComponent,
-    canActivate: [AuthGuard]
-  },
-  { path: 'loan/create',
-    component: LoanCreateComponent,
-    canActivate: [AuthGuard]
-  }
+    { path: '',
+        component: AppComponent,
+        canActivate: [AuthGuard]
+    },
+    { path: 'loan',
+        component: LoanComponent,
+        canActivate: [AuthGuard]
+    },
+    { path: 'loan/create',
+        component: LoanCreateComponent,
+        canActivate: [AuthGuard]
+    }
 ];
 
 @NgModule({
